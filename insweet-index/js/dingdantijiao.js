bd4b7f476ed20aba3ad1dcf0b77d45c21d231ea4
121595113@qq.com <OQOYd3u*puff>
2015-05-22 10:36:10 +0800

    订单提交页完成

--- insweet-index/js/dingdantijiao.js
+++ insweet-index/js/dingdantijiao.js
@@ -1,5 +1,6 @@
 $(function() {
-	function a() {
+	//收货地址
+	function shh_address() {
 		$('.list li').each(function() {
 			var $this = $(this)
 			if ($this.find('input').attr('checked')) {
@@ -9,23 +10,23 @@ $(function() {
 			}
 		});
 	}
-	a();
+	shh_address();
 	$('.shop_car .address').click(function() {
 		if ($(this).hasClass('open')) {
 			$(this).removeClass('open');
-			$('.add_address').hide()
-			a();
-
+			$('.add_address').hide();
+			shh_address()
 		} else {
 			$(this).addClass('open');
 			$('.list li').show();
 			$('.add_address').css('display','inline-block')
 		}
-	})
+	});
+	//支付方式
 	$('.zhifu').click(function(){
 		if ($(this).hasClass('open')) {
 			$(this).removeClass('open');
-			$('.select_pay ul').hide();
+			$('.select_pay ul').hide()
 		} else{
 			$(this).addClass('open');
 			$('.select_pay ul').show()
