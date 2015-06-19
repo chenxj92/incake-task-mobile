18c774c36d9c914adb17dc7606445e4f71c4ab03
121595113@qq.com <OQOYd3u*puff>
2015-06-19 14:10:01 +0800

    修改订单提交页效果

--- insweet-index/js/dingdantijiao.js
+++ insweet-index/js/dingdantijiao.js
@@ -10,12 +10,13 @@ $(function() {
 			}
 		});
 	}
-	function shh_zf () {
-		$('.select_pay li').each(function(){
-			var $this=$(this)
+
+	function shh_zf() {
+		$('.select_pay li').each(function() {
+			var $this = $(this)
 			if ($this.find('input').attr('checked')) {
 				$this.show();
-			} else{
+			} else {
 				$this.hide();
 			}
 		})
@@ -30,26 +31,41 @@ $(function() {
 		} else {
 			$(this).addClass('open');
 			$('.list li').show();
-			$('.add_address').css('display','inline-block')
+			$('.add_address').css('display', 'inline-block')
 		}
 	});
 	//支付方式
-	$('.zhifu').click(function(){
+	$('.zhifu').click(function() {
 		if ($(this).hasClass('open')) {
 			$(this).removeClass('open');
 			shh_zf();
-		} else{
+		} else {
 			$(this).addClass('open');
 			$('.select_pay li').show();
 		}
 	});
-	$('.yhxx h5,.djkq h5').click(function(){
+	$('.yhxx h5,.djkq h5').click(function() {
 		if ($(this).hasClass('open')) {
 			$(this).removeClass('open');
 			$(this).parents('.box').find('.tab_content').hide()
-		} else{
+		} else {
 			$(this).addClass('open');
 			$(this).parents('.box').find('.tab_content').show()
 		}
+	})
+
+	//判断收货人是否为订货人
+	function fn_check () {
+		if ($('#consignee-switch').attr('checked')) {
+			$('#name').parent().hide();
+			$('#phone').parent().hide();
+		} else {
+			$('#name').parent().show();
+			$('#phone').parent().show();
+		}
+	};
+	fn_check();
+	$('#consignee-switch').click(function() {
+		fn_check();
 	})
 });
