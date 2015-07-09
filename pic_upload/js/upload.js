d0698ad992f4d3593f9b0ca0648123d99f67d857
121595113@qq.com <121595113@qq.com>
2015-07-09 22:18:45 +0800

    新增图片预览

--- pic_upload/js/upload.js
+++ pic_upload/js/upload.js
@@ -9,15 +9,40 @@ define(['jquery', 'zepto'], function(jquery, zepto) {
 		$('.preview-small-box').click(function() {
 			//创建蒙版图层
 			$("<div>").prependTo($('body')).addClass('ui-mask');
-			
-			//创建图片
-			var Img = new Image();
-			Img.src = "../img/ditu.jpg";
-			$('<img>').prependTo($('body')).attr("src",Img.src)
 
+			//创建图片
+
+			var imgsrc = "./img/ditu.jpg",
+				zWin = $(window),
+				Img = new Image();
+			Img.src = imgsrc;
+			Img.onload = function() {
+				var w = this.width,
+					h = this.height,
+					winWidth = zWin.width(),
+					winHeight = zWin.height(),
+					realw = parseInt((winWidth - winHeight * w / h) / 2),
+					realh = parseInt((winHeight - winWidth * h / w) / 2);
+				$('<img>').prependTo($('.ui-mask')).attr("src", Img.src).addClass('large-img').css({
+					'width': 'auto',
+					'height': 'auto'
+				});
+				if (h / w > 1.2) {
+					$('.large-img').css({
+						'height': winHeight,
+						'padding-left': +realw + 'px'
+					})
+				} else {
+					$('.large-img').css({
+						'height': winWidth,
+						'padding-top': +realh + 'px'
+					})
+				}
+
+			};
 		});
 		$('.ui-mask').live('tap', function() {
 			$(this).remove()
 		})
-	})
+	});
 });
