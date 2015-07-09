fa97614723a725ef7796a7cb274328661b9142d1
121595113@qq.com <OQOYd3u*puff>
2015-07-09 18:03:40 +0800

    生成图片js

--- pic_upload/js/main.js
+++ pic_upload/js/main.js
@@ -1,17 +1,8 @@
 require.config({
 	paths: {
-		"jquery": "jquery-1.8.2.min",
-		"zepto": "zepto.min"
+		"pre-img": "upload"
 	}
 })
-require(['jquery', 'zepto'], function(jquery, zepto) {
-	$(function() {
-		$('.preview-small-box').click(function() {
-			var Div = $("<div>").prependTo($('body'))
-				.addClass('ui-mask')
-		});
-		$('.ui-mask').live('tap',function(){
-			$(this).remove()
-		})
-	})
+require(['pre-img'], function() {
+	
 });
