91aa75b9199cfcde64987a0142b97557838d539d
121595113@qq.com <OQOYd3u*puff>
2015-07-31 18:02:50 +0800

    更新js

--- insweet-index/js/order-submit-new.js
+++ insweet-index/js/order-submit-new.js
@@ -1,6 +1,38 @@
-$(function(){
-	$("#dd").change(function(){
-		var s=$(this)
-		$('#aa').html(s.val())
-	})
+$(function() {
+	//区域选择
+	$(".select select").change(function() {
+		var Txt = $(this).find("option:selected").text();
+		$(this).parents('.select').find('input').val(Txt);
+	});
+	//配送时间
+	$(".time input").change(function() {
+		var Time = $(this).val()
+		$(this).parent().find('i').html(Time)
+	});
+	//判断订货人和收货人是否为同一人
+	function hecard() {
+		if ($("#hecard").hasClass('ui-radio-on')) {
+			$('#name').parent().hide();
+			$('#phone').parent().hide();
+		} else {
+			$('#name').parent().show();
+			$('#phone').parent().show();
+		}
+	};
+	hecard();
+	$("#hecard").click(function() {
+		hecard();
+	});
+	//代金卡券
+	function djkq() {
+		if ($("#djkq").hasClass('ui-radio-on')) {
+			$('.kq-msg').show();
+		} else {
+			$('.kq-msg').hide();
+		}
+	};
+	djkq();
+	$("#djkq").click(function() {
+		djkq();
+	});
 })
